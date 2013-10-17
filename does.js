1b85d3db78580c0c6a6a72fbfccbd7cf0e1e75c2
unknown <9558109@gmail.com>
2013-10-17 16:29:36 +0100

    refactored does.js, plus formatting changes. Closes #3
    Deliberately refreshes one second late.

--- does.js
+++ does.js
@@ -1,97 +1,78 @@
-//TICK TOCK CAPTAIN
-var pageloadtime = moment();
-function now(){var innernow = moment(); return innernow}
+var interesting_time = 17; //5PM
 
-function gentimeout(){
-var curmins = now().clone().format("mm");
-var nrmins = (curmins - (curmins % 15) + 15)
-var nrtime = now().clone().minutes(nrmins).seconds(0);
-var timeoutmins = nrtime.minutes() - pageloadtime.minutes();
-var timeoutms = timeoutmins*60*1000;
-return timeoutms
+/**finds milliseconds until next refresh needed if you want to refresh every rt minutes*/
+function ms_until_refresh(rt){
+	if (rt==undefined){
+		rt=15;
+	}
+	//get current time
+	var calltime = moment();
+	//calculate time until next upcoming rt
+	var ms_until = 1000*(((((rt - (calltime.minutes() % rt))) - 1) * 60) + (60 - calltime.seconds()));	
+	return ms_until;
 };
 
-var getfresh=setTimeout(function(){find()},gentimeout());
-
-//takes an array, returns an array composed of an alphabetically sorted version of that array with no duplicates
-function sort_unique(arr) {
+/**takes an array and sorts into alphabetical order whilst removing duplicates*/
+function sort_unique(arr){
 	arr = arr.sort();
-	var ret = [arr[0]];
+	var u_arr = [arr[0]];
 	for (var i = 1; i < arr.length; i++) { // start loop at 1 as element 0 can never be a duplicate
 		if (arr[i-1] !== arr[i]) {
-			ret.push(arr[i]);
+			u_arr.push(arr[i]);
 		}
 	}
-	return ret;
-}
+	return u_arr;
+};
 
-function find(){
-	//get the time from the last time this was called
-	var actualtime=now().clone();
-	//initialise arrays used below
-	var raw_places = new Array();
-	var raw_times = new Array();
-	var raw_drinkingcountries = new Array();
-	var drinkingcountries = new Array();
-	var drinkingtimes = new Array();
+/**takes an array of gmt places and an array of countries and updates appropriately-named html entities.*/
+function updatehtml(gmtarr, countryarr){
+	var country_separator = "</span><span class='countrystringseparator'> - </span><span class='countrystringitem'>";
+	var gmtstring = new String;
+	var countrystring = new String;
+	
+	document.getElementById("prefix").innerHTML="It's 5 o'clock in...";
 
-	//give strings some default values
-	var drinkingtimestring = "GMT";
-	var drinkingcountriesstring = "";
-
-	//define the character(s) that seperate(s) the listed countries.
-	var dcsseparator = " - ";
-
-	//gets places where it's 5pm
-	for (var key in zonelist) {
-		if (zonelist.hasOwnProperty(key)) {
-			thislocaltime = moment().tz(key).hours();
-			if (thislocaltime == 17){
-				raw_places.push(key);
-			}
-		}
-	};
-
-	//makes an array of country names and formatted GMT-offset "zones" from the timezone identifiers found above. ?TODO: merge into above loop?
-	for (var i in raw_places){
-		var thiszid = raw_places[i];
-		if (zidtocountry.hasOwnProperty(thiszid)){
-			raw_drinkingcountries.push(zidtocountry[thiszid][0]);
-		}
-		raw_times.push(moment.tz(thiszid).format("Z"));
-	};
-
-	//drinkingcountries is one of our major exports here. This generates a sorted, nonduplicative array from the country names found above.
-	drinkingcountries = sort_unique(raw_drinkingcountries);
-
-	//drinkingtimes is our other primary export. This generates a sorted, nonduplicative array from the GMT offsets found above.
-	drinkingtimes = sort_unique(raw_times);
-
-	//drinkingtimestring is a "secondary" output. It's not required for all uses but it's handy to have. Turns the drinkingtimes array into a formatted string of the form "GMT +xx:xx[[, GMT +xx:xx]n and GMT +xx:xx] as required.
-	for (var i in drinkingtimes){
+	//turns each element of gmtarr into a span, comma'd correctly, and updates an element with id "gmtstring".
+	for (var i in gmtarr){
 		if (i < 1){
-			drinkingtimestring = drinkingtimestring.concat(" " + drinkingtimes[i]);
+			gmtstring = gmtstring.concat("<span class='gmtstringitem'>GMT " + gmtarr[i] + "</span>");
 		}
-		else if (i < (drinkingtimes.length-1)){
-			drinkingtimestring = drinkingtimestring.concat(", GMT " + drinkingtimes[i]);
+		else if (i < (gmtarr.length-1)){
+			gmtstring = gmtstring.concat("<span class='gmtstringseparator'>, </span><span class='gmtstringitem'>GMT " + gmtarr[i] + "</span>");
 		}else{
-			drinkingtimestring = drinkingtimestring.concat(" and GMT " + drinkingtimes[i]);
+			gmtstring = gmtstring.concat("<span class='gmtstringseparator'> and </span><span class='gmtstringitem'>GMT " + gmtarr[i] + "</span>");
 		};	
-	}
+	};
+	document.getElementById("gmtstring").innerHTML=gmtstring;
+	
+	//turns each element of countryarr into a span, interspersed with the separator string, and updates an element with id "countrystring".
+	countrystring = "<span class='countrystringitem'>" + (countryarr.join(country_separator)) + "</span>";
+	document.getElementById("countrystring").innerHTML=countrystring;
+};
 
-	//drinkingcountriesstring is a "secondary" output. It's not required for all uses but it's handy to have. Turns the drinkingcountries array into a string with spaces between them. Implementations can define how to separate (eg ", ", " - " etc) by altering dcsseparator.
-	drinkingcountriesstring = (drinkingcountries.join(dcsseparator));
-	
-	
-	//alter pageloadtime (should probably rename, huh?), which will alter the "next refresh" time
-	pageloadtime = actualtime;
-	
-	//make an "array" of results that are addressable by name.
-	var res = {drinkingcountries: drinkingcountries, drinkingtimes: drinkingtimes, drinkingcountriesstring: drinkingcountriesstring, drinkingtimestring: drinkingtimestring, actualtime: actualtime}
-	
-	//let's get this party fukken started
-	document.getElementById("one").innerHTML="It's 5 o'clock in...</p><h1>" + drinkingtimestring + "</h1><h2>" + drinkingcountriesstring + "</h2>";
-	//don't forget to return your array of results!
+/**returns an array of countries in which walltime is a given time*/
+function places_with_walltime(goal_time){
+	var countries = new Array;
+	var timezones = new Array;
+	//for all countries where walltime == goal_time, array_push that country name and timezone into their respective arrays
+	for (var zid in zonelist) {
+		if (zonelist.hasOwnProperty(zid)) {
+			var walltime = moment().tz(zid).hours();
+			if (walltime == goal_time && ztc(zid)!== false){
+				countries.push(ztc(zid));
+				timezones.push(moment.tz(zid).format("Z"));
+			}			
+		}
+	};
+	var res = {countries: countries, timezones: timezones};
 	return res;
 };
-find();
+
+/**updates the html page every round 15 minutes.*/
+function one_updater(){
+	updatehtml(sort_unique(places_with_walltime(interesting_time).timezones), sort_unique(places_with_walltime(interesting_time).countries))
+	
+	window.setTimeout("one_updater()", ms_until_refresh(15));
+	console.log("hello! I am one_updater. calltime is " + moment().format() + " and ms_until is " + ms_until_refresh(15) + " enjoy!");
+};
+one_updater();
